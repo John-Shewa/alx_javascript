3e16a765aeaaf4ac14ef55c2efae29e86f5f6955
John-Shewa <eldi2002@gmail.com>
2023-09-19 22:13:43 +0300

    java

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -2,7 +2,7 @@
 //a class Rectangle that defines a rectangle
 
 module.exports = class Rectangle{
-    constructor(width, height){
+    constructor(w, h){
         this.width = w;
         this.height = h;
     }
