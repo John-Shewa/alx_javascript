11248ad43a11003bc2499c34208e719ed9aeab27
John-Shewa <eldi2002@gmail.com>
2023-09-19 22:44:20 +0300

    java

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,7 +4,7 @@
 module.exports = class Rectangle{
     constructor(w, h){
         if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-            console.log(error)
+            console.log(TypeError)
         } else {
         this.width = w;
         this.height = h;
