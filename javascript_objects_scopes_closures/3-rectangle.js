00f41e781fa8e2a93efa2492f27af45f1fdf0f23
John-Shewa <eldi2002@gmail.com>
2023-09-19 22:55:18 +0300

    java

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -5,9 +5,7 @@ module.exports = class Rectangle{
     constructor(w, h){
         if (w > 0 && h > 0) { [this.width, this.height] = [w, h];}
     }
+    print () {
+        for (let i = 0; i < this.height; i++) console.log("X".repeat(this.width));
+        }
 };
-print() {
-    for (let i = 0; i < this.height; i++) {
-        console.log("X".repeat(this.width));
-    }
-}
