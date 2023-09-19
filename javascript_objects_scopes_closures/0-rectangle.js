597c6cc0b579d00501ac0420b8b87bd7431a91aa
John-Shewa <eldi2002@gmail.com>
2023-09-19 21:56:03 +0300

    java

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const Rectangle1 = class Rectangle {
+class Rectangle {
     constructor(h, w) {
         this.h = h;
         this.w = w;
     }
 };
-const r1 = new Rectangle1();
+const r1 = new Rectangle();
