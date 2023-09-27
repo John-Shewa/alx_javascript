55c9f95f6052f343022732a3d4aaa412335e0d5e
John-Shewa <eldi2002@gmail.com>
2023-09-27 10:27:31 +0300

    network

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -5,20 +5,20 @@ const request = require('request');
 const url = process.argv[2];
 const char_id = 18;
 
-request(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-
-const output = JSON.parse(body);
-
 let count = 0;
 
-for (let result of output.results) {
-    if (result.characters.some((url) => url.endswith(`${char_id}/`))) {
-        count++;
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.log(error);
+    } else {
+        const data = JSON.parse(body);
+        data.results.forEach((film) => {
+            film.characters.forEach((character) => {
+                if (character.includes(char_id)) {
+                    count += 1;
+                }
+            });
+        });
+        console.log(count);
     }
-}
-console.log(count);
 });
